f5d8209dfc80eda5b09779d92b0d58b3cab69551
Nishant Shah <nishant.shah@wwindia.com>
2020-04-04 22:34:41 +0530

    Update lightsail-compose.sh

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -9,3 +9,6 @@ usermod -aG docker ubuntu
 # install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+apt-get update
+apt-get install git
