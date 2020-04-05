a515ccf5f3e01d61a36cfd4063bc459dafa30059
Nishant Shah <nishant.shah@wwindia.com>
2020-04-05 11:16:51 +0530

    Update lightsail-compose.sh

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -10,6 +10,7 @@ usermod -aG docker ubuntu
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-sudo apt update
-sudo apt install git
-sudo apt install nginx
+apt update -y
+apt install git -y
+apt install nginx -y
+
