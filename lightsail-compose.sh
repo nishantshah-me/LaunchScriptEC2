a9bb4a37ba13a7e9fa33e4ed3053de7d8c0ce72c
Nishant Shah <nishant.shah@wwindia.com>
2021-10-11 12:31:57 +0530

    Update lightsail-compose.sh

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -11,8 +11,18 @@ curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compos
 chmod +x /usr/local/bin/docker-compose
 
 #Gitlab runner
-curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+sudo curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
 
-sudo apt install gitlab-runner
+sudo apt install gitlab-runner -y
 
 sudo chmod +x /usr/local/bin/gitlab-runner
+
+sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+
+sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+
+sudo gitlab-runner start
+
+sudo usermod -a -G sudo gitlab-runner
+
+sudo gitlab-runner register --url=https://gitlab.com/ ---token=ssobXxYFPoQ-FDgGUXrG
